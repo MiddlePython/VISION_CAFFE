f705387c05d1746740542967349195222881fdfc
BOSS_KABAN <nicht0nichts0@gmail.com>
2026-09-11 10:18:23 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.univer"
+        applicationId = "com.example.univer.pos"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
